403b7dfd259b69fa291c407ac7ed975e01f9f851
EetuLeppanen <64865883+EetuLeppanen@users.noreply.github.com>
2020-11-03 15:06:56 +0200

    Update KyselyFetch.js

--- src/components/KyselyFetch.js
+++ src/components/KyselyFetch.js
@@ -2,16 +2,15 @@ import React, { useState, useEffect } from 'react';
 function KyselyFetch () {
 const [tiedot, setTiedot] = useState([]);
 const [teksti, setTeksti] = useState('Haetaan');
-
 const fetchUrl = async () => {
 
 try {
     let url = 'https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaHospitalData';
     const response = await fetch(url);
     const json = await response.json();
-    setTiedot(json.hospitalised);
+    setTiedot(json.question);
     setTeksti('');
-    console.log(json.hospitalised)
+    console.log(json.question)
 
 } catch (error) {
     setTeksti('Haku ei onnistunut');
@@ -23,13 +22,11 @@ useEffect( () => { fetchUrl(); }, [])
 
 return ( <div>
     {
-        tiedot.map( hospitalised => {
-            if(hospitalised.area === "Finland")
+        tiedot.map( question => {
+            if(question.id === "Finland")
             return ( 
-                <div key ={ hospitalised.date}> <br/>
-                    Päivä: {hospitalised.date.substring(0,10)}<br/> Paikka: {hospitalised.area}<br/>
-                    Sairaalassa: {hospitalised.totalHospitalised}, joista osastolla {hospitalised.inWard}    &nbsp; 
-                    ja tehohoidossa {hospitalised.inIcu} 
+                <div key ={ question.id}> <br/>
+                    paska perse :D
 
                     </div>
             );
